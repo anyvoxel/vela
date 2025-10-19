4494112b437ce45a9df476ab8d6c602c9dfceb8a
lsytj0413 <511121939@qq.com>
2025-10-19 16:39:03 +0800

    chore: add github actions

--- main.go
+++ main.go
@@ -3,9 +3,10 @@ package main
 
 import (
 	"context"
-	"fmt"
+	"encoding/json"
 	"log/slog"
 	"os"
+	"path"
 	"time"
 
 	md "github.com/JohannesKaufmann/html-to-markdown"
@@ -14,17 +15,63 @@ import (
 	"github.com/gocolly/colly/v2"
 )
 
-func main() {
+// SummaryAgent is a agent that can summarize a blog post.
+type SummaryAgent struct {
+	agent        *blades.Agent
+	systemPrompt string
+}
+
+// NewSummaryAgent creates a new SummaryAgent.
+func NewSummaryAgent() (*SummaryAgent, error) {
 	systemPrompt, err := os.ReadFile("./system_prompts.txt")
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 
 	agent := blades.NewAgent("Summary Agent",
 		blades.WithModel(os.Getenv("OPENAI_MODEL")), blades.WithProvider(bladesopenai.NewChatProvider()))
 
+	return &SummaryAgent{
+		agent:        agent,
+		systemPrompt: string(systemPrompt),
+	}, nil
+}
+
+// Summary summarizes the given content.
+func (a *SummaryAgent) Summary(ctx context.Context, content string) (string, error) {
+	prompt, err := blades.NewPromptTemplate().System(
+		a.systemPrompt, nil).User("Please summarize the following blog post: {{.context}}", map[string]any{
+		"context": content,
+	}).Build()
+	if err != nil {
+		return "", err
+	}
+
+	result, err := a.agent.Run(ctx, prompt)
+	if err != nil {
+		return "", err
+	}
+
+	return result.Text(), nil
+}
+
+// SummaryResult is the result of a summary.
+type SummaryResult struct {
+	Title       string    `json:"title"`
+	Summary     string    `json:"summary"`
+	PublishedAt time.Time `json:"published_at"`
+}
+
+func main() {
+	summaryAgent, err := NewSummaryAgent()
+	if err != nil {
+		panic(err)
+	}
+
 	listCollector := colly.NewCollector()
 	articleCollector := colly.NewCollector()
+
+	results := make([]*SummaryResult, 0)
 
 	listCollector.OnHTML("div.Blog", func(h *colly.HTMLElement) {
 		h.ForEachWithBreak("div.blog-posts div.post", func(_ int, h *colly.HTMLElement) bool {
@@ -33,7 +80,7 @@ func main() {
 				return false
 			}
 
-			fmt.Printf("process article: %s\n", path)
+			slog.InfoContext(context.Background(), "collect article", slog.String("Path", path))
 			err := articleCollector.Request("GET", path, nil, h.Request.Ctx, nil)
 			if err != nil {
 				slog.ErrorContext(context.TODO(),
@@ -68,22 +115,15 @@ func main() {
 			return
 		}
 
-		fmt.Printf("time: %v\n", t.UTC())
-
-		params := map[string]any{
-			"context": bodyMarkdown,
-		}
-		prompt, err := blades.NewPromptTemplate().System(
-			string(systemPrompt), params).User("Please summarize the following blog post: {{.context}}", params).Build()
-		if err != nil {
-			return
-		}
-
-		result, err := agent.Run(context.Background(), prompt)
+		result, err := summaryAgent.Summary(context.Background(), bodyMarkdown)
 		if err != nil {
 			panic(err)
 		}
-		fmt.Printf("title: %s, summary: %s\n", title, result.Text())
+		results = append(results, &SummaryResult{
+			Title:       title,
+			Summary:     result,
+			PublishedAt: t.UTC(),
+		})
 	})
 
 	err = listCollector.Request("GET", "http://muratbuffalo.blogspot.com", nil, colly.NewContext(), nil)
@@ -94,5 +134,51 @@ func main() {
 	listCollector.Wait()
 	articleCollector.Wait()
 
-	println("Hello, World!")
+	if len(results) == 0 {
+		return
+	}
+
+	monthStr := time.Now().UTC().Format("200601")
+	_, err = os.Stat(path.Join("./data", monthStr))
+	if err != nil {
+		if os.IsNotExist(err) {
+			err = os.MkdirAll(path.Join("./data", monthStr), 0755)
+			if err != nil {
+				panic(err)
+			}
+		} else {
+			panic(err)
+		}
+	}
+
+	dataStr := time.Now().UTC().Format("20060102")
+	filename := path.Join("./data", monthStr, dataStr+".jsonl")
+	var f *os.File
+	_, err = os.Stat(filename)
+	if err != nil {
+		if os.IsNotExist(err) {
+			f, err = os.Create(filename)
+			if err != nil {
+				panic(err)
+			}
+		} else {
+			panic(err)
+		}
+	} else {
+		f, err = os.OpenFile(filename, os.O_APPEND|os.O_WRONLY, 0644)
+		if err != nil {
+			panic(err)
+		}
+	}
+	defer f.Close()
+
+	for _, result := range results {
+		err = json.NewEncoder(f).Encode(result)
+		if err != nil {
+			panic(err)
+		}
+	}
+	slog.InfoContext(context.Background(), "save results",
+		slog.String("Filename", filename),
+		slog.Int("Rows", len(results)))
 }
