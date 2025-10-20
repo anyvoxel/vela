ee30ca26345a11b5f3e3cf8c9bc534ef54f76858
lsytj0413 <511121939@qq.com>
2025-10-20 11:52:23 +0800

    opt: reorg project layout

--- main.go
+++ main.go
@@ -3,67 +3,17 @@ package main
 
 import (
 	"context"
-	"encoding/json"
 	"log/slog"
-	"os"
-	"path"
 	"time"
 
 	md "github.com/JohannesKaufmann/html-to-markdown"
-	"github.com/go-kratos/blades"
-	bladesopenai "github.com/go-kratos/blades/contrib/openai"
+	"github.com/anyvoxel/vela/pkg/agents"
+	"github.com/anyvoxel/vela/pkg/storage"
 	"github.com/gocolly/colly/v2"
 )
 
-// SummaryAgent is a agent that can summarize a blog post.
-type SummaryAgent struct {
-	agent        *blades.Agent
-	systemPrompt string
-}
-
-// NewSummaryAgent creates a new SummaryAgent.
-func NewSummaryAgent() (*SummaryAgent, error) {
-	systemPrompt, err := os.ReadFile("./system_prompts.txt")
-	if err != nil {
-		return nil, err
-	}
-
-	agent := blades.NewAgent("Summary Agent",
-		blades.WithModel(os.Getenv("OPENAI_MODEL")), blades.WithProvider(bladesopenai.NewChatProvider()))
-
-	return &SummaryAgent{
-		agent:        agent,
-		systemPrompt: string(systemPrompt),
-	}, nil
-}
-
-// Summary summarizes the given content.
-func (a *SummaryAgent) Summary(ctx context.Context, content string) (string, error) {
-	prompt, err := blades.NewPromptTemplate().System(
-		a.systemPrompt, nil).User("Please summarize the following blog post: {{.context}}", map[string]any{
-		"context": content,
-	}).Build()
-	if err != nil {
-		return "", err
-	}
-
-	result, err := a.agent.Run(ctx, prompt)
-	if err != nil {
-		return "", err
-	}
-
-	return result.Text(), nil
-}
-
-// SummaryResult is the result of a summary.
-type SummaryResult struct {
-	Title       string    `json:"title"`
-	Summary     string    `json:"summary"`
-	PublishedAt time.Time `json:"published_at"`
-}
-
 func main() { //nolint
-	summaryAgent, err := NewSummaryAgent()
+	summaryAgent, err := agents.NewSummarizer(context.Background())
 	if err != nil {
 		panic(err)
 	}
@@ -71,7 +21,7 @@ func main() { //nolint
 	listCollector := colly.NewCollector()
 	articleCollector := colly.NewCollector()
 
-	results := make([]*SummaryResult, 0)
+	results := make([]*storage.SummaryResult, 0)
 
 	listCollector.OnHTML("div.Blog", func(h *colly.HTMLElement) {
 		h.ForEachWithBreak("div.blog-posts div.post", func(_ int, h *colly.HTMLElement) bool {
@@ -119,7 +69,7 @@ func main() { //nolint
 		if err != nil {
 			panic(err)
 		}
-		results = append(results, &SummaryResult{
+		results = append(results, &storage.SummaryResult{
 			Title:       title,
 			Summary:     result,
 			PublishedAt: t.UTC(),
@@ -134,51 +84,9 @@ func main() { //nolint
 	listCollector.Wait()
 	articleCollector.Wait()
 
-	if len(results) == 0 {
-		return
-	}
-
-	monthStr := time.Now().UTC().Format("200601")
-	_, err = os.Stat(path.Join("./data", monthStr))
+	err = storage.Write(context.Background(), results)
 	if err != nil {
-		if os.IsNotExist(err) {
-			err = os.MkdirAll(path.Join("./data", monthStr), 0755)
-			if err != nil {
-				panic(err)
-			}
-		} else {
-			panic(err)
-		}
+		panic(err)
 	}
-
-	dataStr := time.Now().UTC().Format("20060102")
-	filename := path.Join("./data", monthStr, dataStr+".jsonl")
-	var f *os.File
-	_, err = os.Stat(filename)
-	if err != nil {
-		if os.IsNotExist(err) {
-			f, err = os.Create(filename)
-			if err != nil {
-				panic(err)
-			}
-		} else {
-			panic(err)
-		}
-	} else {
-		f, err = os.OpenFile(filename, os.O_APPEND|os.O_WRONLY, 0644)
-		if err != nil {
-			panic(err)
-		}
-	}
-	defer f.Close() //nolint
-
-	for _, result := range results {
-		err = json.NewEncoder(f).Encode(result)
-		if err != nil {
-			panic(err)
-		}
-	}
-	slog.InfoContext(context.Background(), "save results",
-		slog.String("Filename", filename),
-		slog.Int("Rows", len(results)))
+	slog.InfoContext(context.Background(), "process done")
 }
---
+++ pkg/agents/agent.go
@@ -0,0 +1,49 @@
+// Package agents contains implementation of llm summarizer.
+package agents
+
+import (
+	"context"
+	_ "embed"
+	"os"
+
+	"github.com/go-kratos/blades"
+	bladesopenai "github.com/go-kratos/blades/contrib/openai"
+)
+
+//go:embed system_prompts.md
+var systemPrompts string
+
+// Summarizer is a agent that can summarize a blog post.
+type Summarizer struct {
+	agent        *blades.Agent
+	systemPrompt string
+}
+
+// NewSummarizer creates a new summarizer agent.
+func NewSummarizer(_ context.Context) (*Summarizer, error) {
+	agent := blades.NewAgent("Summary Agent",
+		blades.WithModel(os.Getenv("OPENAI_MODEL")), blades.WithProvider(bladesopenai.NewChatProvider()))
+
+	return &Summarizer{
+		agent:        agent,
+		systemPrompt: systemPrompts,
+	}, nil
+}
+
+// Summary summarizes the given content.
+func (a *Summarizer) Summary(ctx context.Context, content string) (string, error) {
+	prompt, err := blades.NewPromptTemplate().System(
+		a.systemPrompt, nil).User("Please summarize the following blog post: {{.context}}", map[string]any{
+		"context": content,
+	}).Build()
+	if err != nil {
+		return "", err
+	}
+
+	result, err := a.agent.Run(ctx, prompt)
+	if err != nil {
+		return "", err
+	}
+
+	return result.Text(), nil
+}
