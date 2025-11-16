e262d604165717ac65222498447514052bb27614
lsytj0413 <511121939@qq.com>
2025-11-16 21:05:42 +0800

    feat: add source brendangregg and emptysqua

---
+++ pkg/collectors/brendangregg/collector.go
@@ -0,0 +1,118 @@
+// Package brendangregg implement collector for https://www.brendangregg.com/blog/index.html
+package brendangregg
+
+import (
+	"context"
+	"log/slog"
+	"reflect"
+	"time"
+
+	md "github.com/JohannesKaufmann/html-to-markdown"
+	"github.com/anyvoxel/airmid/anvil"
+	airapp "github.com/anyvoxel/airmid/app"
+	"github.com/anyvoxel/airmid/ioc"
+	"github.com/gocolly/colly/v2"
+
+	"github.com/anyvoxel/vela/pkg/collectors"
+)
+
+func init() {
+	anvil.Must(airapp.RegisterBeanDefinition(
+		"vela.collectors.brendangregg",
+		ioc.MustNewBeanDefinition(
+			reflect.TypeOf((*Collector)(nil)),
+		),
+	))
+}
+
+// Collector implemetation.
+type Collector struct {
+	listCollector *colly.Collector
+	postCollector *colly.Collector
+}
+
+var (
+	_ ioc.InitializingBean = (*Collector)(nil)
+	_ collectors.Collector = (*Collector)(nil)
+)
+
+// AfterPropertiesSet implement InitializingBean
+func (c *Collector) AfterPropertiesSet() error {
+	c.listCollector = colly.NewCollector()
+	c.postCollector = colly.NewCollector()
+	return nil
+}
+
+// Name implement collector.Name
+func (c *Collector) Name() string {
+	return "brendangregg"
+}
+
+// Initialize implement collector.Initialize
+func (c *Collector) Initialize(_ context.Context) error {
+	return nil
+}
+
+// Start implement collector.Start
+func (c *Collector) Start(ctx context.Context, ch chan<- collectors.Post) error {
+	defer close(ch)
+
+	c.listCollector.OnHTML("#home", func(h *colly.HTMLElement) {
+		h.ForEachWithBreak("li", func(_ int, h *colly.HTMLElement) bool {
+			path := h.ChildAttr("a", "href")
+			if path == "" {
+				return false
+			}
+
+			slog.InfoContext(ctx, "collect article", slog.String("Path", path))
+			err := c.postCollector.Request("GET", "https://www.brendangregg.com/"+path, nil, nil, nil)
+			if err != nil {
+				slog.ErrorContext(ctx,
+					"cann't request on article",
+					slog.Any("Error", err),
+					slog.String("NextPath", path))
+				return false
+			}
+
+			return true
+		})
+	})
+
+	c.postCollector.OnHTML("div.site", func(h *colly.HTMLElement) {
+		title := h.ChildText("h2.big")
+		if title == "" {
+			return
+		}
+
+		publishedAt := h.ChildText("p.meta")
+		t, err := time.Parse("02 Jan 2006", publishedAt)
+		if err != nil {
+			return
+		}
+
+		postBody, err := h.DOM.Find("div.post").Html()
+		if err != nil {
+			return
+		}
+		bodyMarkdown, err := md.NewConverter("", true, nil).ConvertString(postBody)
+		if err != nil {
+			return
+		}
+		ch <- collectors.Post{
+			Domain:      c.Name(),
+			Title:       title,
+			Path:        h.Request.URL.String(),
+			PublishedAt: t,
+			Content:     bodyMarkdown,
+		}
+	})
+
+	err := c.listCollector.Request("GET", "https://www.brendangregg.com/blog/index.html", nil, colly.NewContext(), nil)
+	if err != nil {
+		return err
+	}
+
+	c.listCollector.Wait()
+	c.postCollector.Wait()
+	return nil
+}
