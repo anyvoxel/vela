f10ba1241edaf2fe154fc819ab593c02c9905ab2
lsytj0413 <511121939@qq.com>
2025-11-17 14:54:14 +0800

    feat: add source davidxiang, googlepubs, kaiwaehner

---
+++ pkg/collectors/googlepubs/collector.go
@@ -0,0 +1,112 @@
+// Package googlepubs implement collector for https://research.google/pubs/
+package googlepubs
+
+import (
+	"context"
+	"log/slog"
+	"reflect"
+	"time"
+
+	md "github.com/JohannesKaufmann/html-to-markdown"
+	"github.com/anyvoxel/airmid/anvil"
+	airapp "github.com/anyvoxel/airmid/app"
+	"github.com/anyvoxel/airmid/ioc"
+	"github.com/gocolly/colly/v2"
+
+	"github.com/anyvoxel/vela/pkg/collectors"
+)
+
+func init() {
+	anvil.Must(airapp.RegisterBeanDefinition(
+		"vela.collectors.googlepubs",
+		ioc.MustNewBeanDefinition(
+			reflect.TypeOf((*Collector)(nil)),
+		),
+	))
+}
+
+// Collector implemetation.
+type Collector struct {
+	listCollector *colly.Collector
+	postCollector *colly.Collector
+}
+
+var (
+	_ ioc.InitializingBean = (*Collector)(nil)
+	_ collectors.Collector = (*Collector)(nil)
+)
+
+// AfterPropertiesSet implement InitializingBean
+func (c *Collector) AfterPropertiesSet() error {
+	c.listCollector = colly.NewCollector()
+	c.postCollector = colly.NewCollector()
+	return nil
+}
+
+// Name implement collector.Name
+func (c *Collector) Name() string {
+	return "googlepubs"
+}
+
+// Initialize implement collector.Initialize
+func (c *Collector) Initialize(_ context.Context) error {
+	return nil
+}
+
+// Start implement collector.Start
+func (c *Collector) Start(ctx context.Context, ch chan<- collectors.Post) error {
+	defer close(ch)
+
+	c.listCollector.OnHTML("div.list-wrapper", func(h *colly.HTMLElement) {
+		h.ForEachWithBreak("div.publications-list", func(_ int, h *colly.HTMLElement) bool {
+			path := h.ChildAttr("div.row-card__body a", "href")
+			if path == "" {
+				return false
+			}
+
+			slog.InfoContext(ctx, "collect article", slog.String("Path", path))
+			err := c.postCollector.Request("GET", path, nil, nil, nil)
+			if err != nil {
+				slog.ErrorContext(ctx,
+					"cann't request on article",
+					slog.Any("Error", err),
+					slog.String("NextPath", path))
+				return false
+			}
+
+			return true
+		})
+	})
+
+	c.postCollector.OnHTML("#page-content", func(h *colly.HTMLElement) {
+		title := h.ChildText("h1.headline-2")
+		if title == "" {
+			return
+		}
+
+		postBody, err := h.DOM.Find("section.offset-two-up").Html()
+		if err != nil {
+			return
+		}
+		bodyMarkdown, err := md.NewConverter("", true, nil).ConvertString(postBody)
+		if err != nil {
+			return
+		}
+		ch <- collectors.Post{
+			Domain:      c.Name(),
+			Title:       title,
+			Path:        h.Request.URL.String(),
+			PublishedAt: time.Time{},
+			Content:     bodyMarkdown,
+		}
+	})
+
+	err := c.listCollector.Request("GET", "https://research.google/pubs/", nil, colly.NewContext(), nil)
+	if err != nil {
+		return err
+	}
+
+	c.listCollector.Wait()
+	c.postCollector.Wait()
+	return nil
+}
---
+++ pkg/collectors/kaiwaehner/collector.go
@@ -0,0 +1,118 @@
+// Package kaiwaehner implement collector for https://www.kai-waehner.de/
+package kaiwaehner
+
+import (
+	"context"
+	"log/slog"
+	"reflect"
+	"time"
+
+	md "github.com/JohannesKaufmann/html-to-markdown"
+	"github.com/anyvoxel/airmid/anvil"
+	airapp "github.com/anyvoxel/airmid/app"
+	"github.com/anyvoxel/airmid/ioc"
+	"github.com/gocolly/colly/v2"
+
+	"github.com/anyvoxel/vela/pkg/collectors"
+)
+
+func init() {
+	anvil.Must(airapp.RegisterBeanDefinition(
+		"vela.collectors.kaiwaehner",
+		ioc.MustNewBeanDefinition(
+			reflect.TypeOf((*Collector)(nil)),
+		),
+	))
+}
+
+// Collector implemetation.
+type Collector struct {
+	listCollector *colly.Collector
+	postCollector *colly.Collector
+}
+
+var (
+	_ ioc.InitializingBean = (*Collector)(nil)
+	_ collectors.Collector = (*Collector)(nil)
+)
+
+// AfterPropertiesSet implement InitializingBean
+func (c *Collector) AfterPropertiesSet() error {
+	c.listCollector = colly.NewCollector()
+	c.postCollector = colly.NewCollector()
+	return nil
+}
+
+// Name implement collector.Name
+func (c *Collector) Name() string {
+	return "kaiwaehner"
+}
+
+// Initialize implement collector.Initialize
+func (c *Collector) Initialize(_ context.Context) error {
+	return nil
+}
+
+// Start implement collector.Start
+func (c *Collector) Start(ctx context.Context, ch chan<- collectors.Post) error {
+	defer close(ch)
+
+	c.listCollector.OnHTML("div.post-archive div.archive-main", func(h *colly.HTMLElement) {
+		h.ForEachWithBreak("article.post", func(_ int, h *colly.HTMLElement) bool {
+			path := h.ChildAttr("h2.entry-title a", "href")
+			if path == "" {
+				return false
+			}
+
+			slog.InfoContext(ctx, "collect article", slog.String("Path", path))
+			err := c.postCollector.Request("GET", path, nil, nil, nil)
+			if err != nil {
+				slog.ErrorContext(ctx,
+					"cann't request on article",
+					slog.Any("Error", err),
+					slog.String("NextPath", path))
+				return false
+			}
+
+			return true
+		})
+	})
+
+	c.postCollector.OnHTML("#content", func(h *colly.HTMLElement) {
+		title := h.ChildText("h1.entry-title")
+		if title == "" {
+			return
+		}
+
+		publishedAt := h.ChildText("div.entry-meta-details ul.post-meta li.meta-date")
+		t, err := time.Parse("02. January 2006", publishedAt)
+		if err != nil {
+			return
+		}
+
+		postBody, err := h.DOM.Find("div.entry-content div[dir=auto]").Html()
+		if err != nil {
+			return
+		}
+		bodyMarkdown, err := md.NewConverter("", true, nil).ConvertString(postBody)
+		if err != nil {
+			return
+		}
+		ch <- collectors.Post{
+			Domain:      c.Name(),
+			Title:       title,
+			Path:        h.Request.URL.String(),
+			PublishedAt: t,
+			Content:     bodyMarkdown,
+		}
+	})
+
+	err := c.listCollector.Request("GET", "https://www.kai-waehner.de/", nil, colly.NewContext(), nil)
+	if err != nil {
+		return err
+	}
+
+	c.listCollector.Wait()
+	c.postCollector.Wait()
+	return nil
+}
