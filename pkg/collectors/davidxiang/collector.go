716d0bbb352159b149b398c8a635333d9da92f4f
lsytj0413 <511121939@qq.com>
2025-11-17 14:54:14 +0800

    feat: add source davidxiang, googlepubs, kaiwaehner

---
+++ pkg/collectors/davidxiang/collector.go
@@ -0,0 +1,118 @@
+// Package davidxiang implement collector for https://davidxiang.com/
+package davidxiang
+
+import (
+	"context"
+	"log/slog"
+	"reflect"
+	"time"
+
+	md "github.com/JohannesKaufmann/html-to-markdown"
+	"github.com/anyvoxel/airmid/anvil"
+	airapp "github.com/anyvoxel/airmid/app"
+	"github.com/anyvoxel/airmid/ioc"
+	"github.com/gocolly/colly/v2"
+
+	"github.com/anyvoxel/vela/pkg/collectors"
+)
+
+func init() {
+	anvil.Must(airapp.RegisterBeanDefinition(
+		"vela.collectors.davidxiang",
+		ioc.MustNewBeanDefinition(
+			reflect.TypeOf((*Collector)(nil)),
+		),
+	))
+}
+
+// Collector implemetation.
+type Collector struct {
+	listCollector *colly.Collector
+	postCollector *colly.Collector
+}
+
+var (
+	_ ioc.InitializingBean = (*Collector)(nil)
+	_ collectors.Collector = (*Collector)(nil)
+)
+
+// AfterPropertiesSet implement InitializingBean
+func (c *Collector) AfterPropertiesSet() error {
+	c.listCollector = colly.NewCollector()
+	c.postCollector = colly.NewCollector()
+	return nil
+}
+
+// Name implement collector.Name
+func (c *Collector) Name() string {
+	return "davidxiang"
+}
+
+// Initialize implement collector.Initialize
+func (c *Collector) Initialize(_ context.Context) error {
+	return nil
+}
+
+// Start implement collector.Start
+func (c *Collector) Start(ctx context.Context, ch chan<- collectors.Post) error {
+	defer close(ch)
+
+	c.listCollector.OnHTML("#main", func(h *colly.HTMLElement) {
+		h.ForEachWithBreak("article.post", func(_ int, h *colly.HTMLElement) bool {
+			path := h.ChildAttr("h2.entry-title a", "href")
+			if path == "" {
+				return false
+			}
+
+			slog.InfoContext(ctx, "collect article", slog.String("Path", path))
+			err := c.postCollector.Request("GET", "https://davidxiang.com/"+path, nil, nil, nil)
+			if err != nil {
+				slog.ErrorContext(ctx,
+					"cann't request on article",
+					slog.Any("Error", err),
+					slog.String("NextPath", path))
+				return false
+			}
+
+			return true
+		})
+	})
+
+	c.postCollector.OnHTML("article.post", func(h *colly.HTMLElement) {
+		title := h.ChildText("h1.post-title")
+		if title == "" {
+			return
+		}
+
+		publishedAt := h.ChildText("div.header-standard span time.entry-date")
+		t, err := time.Parse(time.DateOnly, publishedAt)
+		if err != nil {
+			return
+		}
+
+		postBody, err := h.DOM.Find("div.post-entry div.entry-content").Html()
+		if err != nil {
+			return
+		}
+		bodyMarkdown, err := md.NewConverter("", true, nil).ConvertString(postBody)
+		if err != nil {
+			return
+		}
+		ch <- collectors.Post{
+			Domain:      c.Name(),
+			Title:       title,
+			Path:        h.Request.URL.String(),
+			PublishedAt: t,
+			Content:     bodyMarkdown,
+		}
+	})
+
+	err := c.listCollector.Request("GET", "https://davidxiang.com/", nil, colly.NewContext(), nil)
+	if err != nil {
+		return err
+	}
+
+	c.listCollector.Wait()
+	c.postCollector.Wait()
+	return nil
+}
