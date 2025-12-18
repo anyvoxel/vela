3a083615526201c57387a77a7b5cd7541fa4f315
lsytj0413 <511121939@qq.com>
2025-12-18 09:47:11 +0800

    feat: support arpitbhayani collector

---
+++ pkg/collectors/arpitbhayani/collector.go
@@ -0,0 +1,113 @@
+// Package arpitbhayani implement collector for https://arpitbhayani.me/blogs
+package arpitbhayani
+
+import (
+	"context"
+	"log/slog"
+	"reflect"
+	"time"
+
+	md "github.com/JohannesKaufmann/html-to-markdown"
+	"github.com/anyvoxel/airmid/anvil"
+	airapp "github.com/anyvoxel/airmid/app"
+	"github.com/anyvoxel/airmid/ioc"
+	"github.com/gocolly/colly/v2"
+	slogctx "github.com/veqryn/slog-context"
+
+	"github.com/anyvoxel/vela/pkg/collectors"
+)
+
+func init() {
+	anvil.Must(airapp.RegisterBeanDefinition(
+		"vela.collectors.arpitbhayani",
+		ioc.MustNewBeanDefinition(
+			reflect.TypeOf((*Collector)(nil)),
+		),
+	))
+}
+
+// Collector implemetation.
+type Collector struct {
+	listCollector *colly.Collector
+	postCollector *colly.Collector
+}
+
+var (
+	_ ioc.InitializingBean = (*Collector)(nil)
+	_ collectors.Collector = (*Collector)(nil)
+)
+
+// AfterPropertiesSet implement InitializingBean
+func (c *Collector) AfterPropertiesSet(context.Context) error {
+	c.listCollector = colly.NewCollector()
+	c.postCollector = colly.NewCollector()
+	return nil
+}
+
+// Name implement collector.Name
+func (c *Collector) Name() string {
+	return "arpitbhayani"
+}
+
+// Initialize implement collector.Initialize
+func (c *Collector) Initialize(_ context.Context) error {
+	return nil
+}
+
+// Start implement collector.Start
+func (c *Collector) Start(ctx context.Context, ch chan<- collectors.Post) error {
+	defer close(ch)
+
+	c.listCollector.OnHTML("body div.section div.content div.content ul", func(h *colly.HTMLElement) {
+		h.ForEachWithBreak("li", func(_ int, h *colly.HTMLElement) bool {
+			path := h.ChildAttr("span a", "href")
+			if path == "" {
+				return false
+			}
+
+			slogctx.FromCtx(ctx).InfoContext(ctx, "collect article", slog.String("Path", path))
+			err := c.postCollector.Request("GET", "https://arpitbhayani.me"+path, nil, nil, nil)
+			if err != nil {
+				slogctx.FromCtx(ctx).ErrorContext(ctx,
+					"cann't request on article",
+					slog.Any("Error", err),
+					slog.String("NextPath", path))
+				return false
+			}
+
+			return true
+		})
+	})
+
+	c.postCollector.OnHTML("body div.section div.columns", func(h *colly.HTMLElement) {
+		title := h.ChildText("div h1.title")
+		if title == "" {
+			return
+		}
+
+		postBody, err := h.DOM.Find("div.blog-content").Html()
+		if err != nil {
+			return
+		}
+		bodyMarkdown, err := md.NewConverter("", true, nil).ConvertString(postBody)
+		if err != nil {
+			return
+		}
+		ch <- collectors.Post{
+			Domain:      c.Name(),
+			Title:       title,
+			Path:        h.Request.URL.String(),
+			PublishedAt: time.Time{},
+			Content:     bodyMarkdown,
+		}
+	})
+
+	err := c.listCollector.Request("GET", "https://arpitbhayani.me/blogs", nil, colly.NewContext(), nil)
+	if err != nil {
+		return err
+	}
+
+	c.listCollector.Wait()
+	c.postCollector.Wait()
+	return nil
+}
